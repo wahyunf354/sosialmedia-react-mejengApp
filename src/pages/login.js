88588b9dba28c1489caa0faae8acf8eb4ca3568d
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-06-08 21:36:44 +0700

    add navbar

---
+++ src/pages/login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class login extends Component {
+  render() {
+    return (
+      <div>
+        <p>login</p>
+      </div>
+    )
+  }
+}
+
+export default login
---
+++ src/pages/signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class signup extends Component {
+  render() {
+    return (
+      <div>
+        <p>signup</p>
+      </div>
+    )
+  }
+}
+
+export default signup
